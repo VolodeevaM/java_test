9f494bd11218c07fc4fc8a8cbf73a39b1931bd16
VolodeevaM <94539028+VolodeevaM@users.noreply.github.com>
2021-11-28 18:06:56 +0300

    Tests
    tests

--- sandbox/src/main/java/ru/vol/sandbox/first.java
+++ sandbox/src/main/java/ru/vol/sandbox/first.java
@@ -4,7 +4,22 @@ public class first {
 
     public static void main(String[] args) {
         System.out.println("hello");
-   
+
+        Square s = new Square(5);
+        System.out.println("Площадь");
+
+        Point p1 = new Point(1, 3, 1, 3);
+
+        System.out.println("Расстояние" + distance (p1));
     }
+
+    public static double area(Square s) {
+        return s.l*s.l;
+    }
+
+    public static double distance (Point p1) {
+        return Math.sqrt((p1.x2 - p1.x1) * (p1.x2 - p1.x1) + (p1.y2 - p1.y1)*(p1.y2 - p1.y1));
+    }
+
 
 }
