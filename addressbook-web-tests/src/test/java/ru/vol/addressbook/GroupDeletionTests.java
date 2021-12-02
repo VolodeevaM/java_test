e34ed86c75839b9ca9268160c8d12cbfad2277ec
VolodeevaM <94539028+VolodeevaM@users.noreply.github.com>
2021-12-02 20:48:49 +0300

    Добавлены тесты для удаления групп

---
+++ addressbook-web-tests/src/test/java/ru/vol/addressbook/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package ru.vol.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase{
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    wd.get("http://localhost:8443/addressbook/group.php");
+    selectGroup();
+    deleteGroup();
+    returnToGroupPage();
+  }
+
+
+}
