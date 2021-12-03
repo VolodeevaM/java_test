44f4de3d15284fe483cb5032f601ef181efd5a99
VolodeevaM <94539028+VolodeevaM@users.noreply.github.com>
2021-12-03 14:01:58 +0300

    общий базовый класс

--- addressbook-web-tests/src/test/java/ru/vol/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/vol/addressbook/appmanager/ApplicationManager.java
@@ -1,59 +1,31 @@
 package ru.vol.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import ru.vol.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    public WebDriver wd;
+
+    FirefoxDriver wd;
+
+    private SessionHelper sessionHelper;
+    private GroupHelper groupHelper;
+
+
+
 
     public void init() {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost:8443/addressbook/group.php");
-        Login("admin", "secret");
+        groupHelper = new GroupHelper(wd);
+        sessionHelper = new SessionHelper(wd);
+        sessionHelper.Login("admin", "secret");
     }
 
-    private void Login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.id("LoginForm")).click();
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
 
-    public void returnToGroupPage() {
-        wd.findElement(By.linkText("group page")).click();
-    }
-
-    public void initGroupCreation() {
-        wd.findElement(By.name("new")).click();
-    }
-
-    public void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
-    }
-
-    public void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wd.findElement(By.xpath("//form[@action='/addressbook/group.php']")).click();
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
 
     public void stop() {
         wd.quit();
@@ -68,20 +40,8 @@ public class ApplicationManager {
         }
     }
 
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 
-    public void deleteGroup() {
-      wd.findElement(By.name("delete")).click();
-    }
-
-    public void selectGroup() {
-      wd.findElement(By.name("selected[]")).click();
+    public GroupHelper getGroupHelper() {
+        return groupHelper;
     }
 }
--- addressbook-web-tests/src/test/java/ru/vol/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/vol/addressbook/tests/GroupDeletionTests.java
@@ -6,10 +6,10 @@ public class GroupDeletionTests extends TestBase{
 
   @Test
   public void testGroupDeletion() throws Exception {
-    app.wd.get("http://localhost:8443/addressbook/group.php");
-    app.selectGroup();
-    app.deleteGroup();
-    app.returnToGroupPage();
+    app.getGroupHelper().wd.get("http://localhost:8443/addressbook/group.php");
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteGroup();
+    app.getGroupHelper().returnToGroupPage();
   }
 
 
