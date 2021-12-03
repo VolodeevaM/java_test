3c7a3067afb5f10c2b5400e41c39a3b613c9b722
VolodeevaM <94539028+VolodeevaM@users.noreply.github.com>
2021-12-03 19:28:50 +0300

    модификация группы

---
+++ addressbook-web-tests/src/test/java/ru/vol/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,16 @@
+package ru.vol.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.vol.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+
+    @Test
+    public void testGroupModification() {
+        app.getGroupHelper().SelectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "2", "3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
