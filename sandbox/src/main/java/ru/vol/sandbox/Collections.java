ac8eb0fb7bd49d9329b6d7fcfe16fdd03ee7f593
VolodeevaM <94539028+VolodeevaM@users.noreply.github.com>
2022-04-18 15:22:41 +0300

    урок 4.2

--- sandbox/src/main/java/ru/vol/sandbox/Collections.java
+++ sandbox/src/main/java/ru/vol/sandbox/Collections.java
@@ -1,10 +1,16 @@
 package ru.vol.sandbox;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
     public static void main(String[] args) {
         String [] langs = {"Java", "C#", "Python", "PHP"};
 
-        for (String l : langs) {
+        List languages = Arrays.asList("Java", "C#++", "Python", "PHP");
+
+
+        for (Object l : languages) {
             System.out.println("Я хочу выучить " + l);
         }
 
